4d4810049645bc00eacf2a9a554ddc7a328cc770
SeherCevik <78930411+SeherCevik@users.noreply.github.com>
2022-06-06 14:13:05 +0300

    local mongodb veri çekişi tamam

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-import {findBlocks} from "./lib/findBlocks";
+const findBlocks = require("./lib/findBlocks");
 
-export default findBlocks;
+
+const MongoClient = require('mongodb').MongoClient;
+const URL ='mongodb://localhost:27017';
+
+MongoClient.connect(URL, (err, client) => {
+    if (err) throw err;
+    const db = client.db('EnergyExchange');
+   
+    let sorgu = {};
+    db.collection('EnergyOffers').find(sorgu).toArray((err, result) => {
+        if (err) throw err;
+        console.log(JSON.stringify(result, null, ' '));
+        client.close();
+    });
+
+});
